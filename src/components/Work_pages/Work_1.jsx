0369167d4ee56eb904c61ca72d3454ccb8000b13
Moodyean <andrefnmatos@gmail.com>
2025-03-21 02:42:15 +0000

    new code_14

--- src/components/Work_pages/Work_1.jsx
+++ src/components/Work_pages/Work_1.jsx
@@ -5,8 +5,8 @@ const Work_1 = () => {
   return (
     <div className='app__work1'>
       <div className='app__work1-content'>
-          <h1>Plate Perfection Project</h1>
-          <p>This is the work page for Plate Perfection.</p>
+          <div className='app__work1-content-title'>Plate Perfection Project</div>
+          <div className='app__work1-content-description'>This is the work page for Plate Perfection.</div>
       </div>
     </div>
   );

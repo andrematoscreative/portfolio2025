e5d36a0150d228683294593cd90ee32538f82867
Moodyean <andrefnmatos@gmail.com>
2025-03-21 02:28:29 +0000

    new code_13

--- src/components/Work_pages/Work_1.jsx
+++ src/components/Work_pages/Work_1.jsx
@@ -3,9 +3,11 @@ import './Work_1.scss'
 
 const Work_1 = () => {
   return (
-    <div>
-      <h1>Plate Perfection Project</h1>
-      <p>This is the work page for Plate Perfection.</p>
+    <div className='app__work1'>
+      <div className='app__work1-content'>
+          <h1>Plate Perfection Project</h1>
+          <p>This is the work page for Plate Perfection.</p>
+      </div>
     </div>
   );
 };
